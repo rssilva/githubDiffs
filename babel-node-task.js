94a5264a09cb695d07943c2aa9605ebd60c4a338
rssilva <rafaelspdasilva@gmail.com>
2015-05-23 22:25:33 -0300

    adding task to a optimized babel transpiling of backend files

--- babel-node-task.js
+++ babel-node-task.js
@@ -5,6 +5,14 @@ var mkdirp = require('mkdirp');
 
 var path = process.argv[2];
 
+var dest = path.replace(/^app\//, './build/');
+dest = dest.replace(/[^\/]{0,}$/, '');
+
+mkdirp(dest, function (err) {
+  if (err) console.error(err)
+  else console.log(dest, ' created!')
+});
+
 babel.transformFile(path, {}, function (err, result) {
   var dest = path.replace(/^app\//, './build/');
 
