644b0f3920533799d110368fc41563ba90a50d79
rssilva <rafaelspdasilva@gmail.com>
2015-05-24 21:40:51 -0300

    adding methods to get by login and get by request

--- app/models/User.js
+++ app/models/User.js
@@ -1,4 +1,42 @@
-"use strict";
+let request = require('request');
+let mongoose = require('mongoose');
+let UserSchemaObj = require('./UserSchema');
+let Schema = mongoose.Schema;
 
-let user = {};
-let laudrup = {};
+let UserSchema = new Schema(UserSchemaObj);
+
+function getByLogin (login) {
+  User.find({ login: login }, (err, result) => {
+    if (err) return console.error(err);
+
+    result.length ? null : getByRequest(login);
+  });
+};
+
+function getByRequest (login) {
+
+  let options = {
+    url: `https://api.github.com/users/${login}`,
+    headers: {
+      'User-Agent': 'diffs app'
+    }
+  }
+
+  request(options, (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      body = JSON.parse(body);
+
+      let user = new User(body);
+
+      user.save((err) => {
+        console.log(err, 'Saved?')
+      });
+    }
+  });
+};
+
+UserSchema.methods.getByLogin = getByLogin;
+
+let User = mongoose.model('users', UserSchema);
+
+module.exports = User;
