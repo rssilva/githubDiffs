596fab280498af19dd642f8d5186210250fef31d
rssilva <rafaelspdasilva@gmail.com>
2015-05-30 21:58:33 -0300

    adding followers routes

---
+++ app/routes/FollowersRoutes.js
@@ -0,0 +1,21 @@
+let Followers = require('../collections/FollowersCollection');
+let followers = new Followers();
+
+let FollowersRoutes = {
+  init(server) {
+
+    server.route({
+      method: 'GET',
+      path: '/users/{login}/followers',
+      handler:function (request, reply) {
+        const login = request.params.login;
+        const page = request.params.page;
+
+        followers.getByLogin(login, page, reply);
+      }
+    });
+
+  }
+}
+
+module.exports = FollowersRoutes;
